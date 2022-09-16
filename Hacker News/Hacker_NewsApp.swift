d9ddf3aad1957ca4ffa0a6e4be5204cc2b71afea
PorkStew <stewartclay166@gmail.com>
2022-09-16 05:00:30 -0700

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Stewart Clay on 2022/09/16.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
